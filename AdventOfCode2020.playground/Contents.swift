2a4633d5ff7e832ca23bcfe16aa433e060708a26
Simon Biickert <simon@biickert.ca>
2020-12-05 12:38:02 -0700

    Day 2 complete, parts 1 and 2.

--- AdventOfCode2020.playground/Contents.swift
+++ AdventOfCode2020.playground/Contents.swift
@@ -2,16 +2,4 @@ import Cocoa
 
 //Day1.solveForTwo()
 //Day1.solveForThree()
-
-//let regex = try NSRegularExpression(pattern: "(\\d+)-(\\d+) (\\w): (\\w+)", options: [])
-//let strEntry = "9-14 z: vzzzpzfzdzzzzzzfczz"
-//let range = NSRange(location: 0, length: strEntry.utf16.count)
-//if let match = regex.firstMatch(in: strEntry, options:[], range: range) {
-//	if match.numberOfRanges == 5 {
-//		if let r = Range(match.range(at: 4), in: strEntry) {
-//			strEntry[r]
-//		}
-//	}
-//}
-
-Day2.solve()
+//Day2.solve()
--- AdventOfCode2020.playground/Sources/Day2.swift
+++ AdventOfCode2020.playground/Sources/Day2.swift
@@ -3,13 +3,22 @@ import Foundation
 public class Day2 {
 	public static func solve() {
 		let entries = readPasswordFile()
+		
 		var validCount = 0
 		for entry in entries {
-			if entry.isValid() {
+			if entry.isValidForPart1() {
 				validCount += 1
 			}
 		}
-		print("There were \(validCount) valid entries.")
+		print("There were \(validCount) valid entries according to Part 1 rules.")
+		
+		validCount = 0
+		for entry in entries {
+			if entry.isValidForPart2() {
+				validCount += 1
+			}
+		}
+		print("There were \(validCount) valid entries according to Part 2 rules.")
 	}
 
 	static func readPasswordFile() -> [PasswordEntry] {
@@ -62,8 +71,23 @@ struct PasswordEntry {
 	let rule: PasswordRule
 	let password: String
 	
-	func isValid() -> Bool {
+	func isValidForPart1() -> Bool {
 		let countChar = password.filter { $0 == rule.character }.count
 		return countChar >= rule.min && countChar <= rule.max
+	}
+	
+	func isValidForPart2() -> Bool {
+		// Note min, max are 1-indexed, so have to subtract 1
+		guard rule.max <= password.count else {
+			return false
+		}
+		let idx1s = password.index(password.startIndex, offsetBy: rule.min-1)
+		let idx1e = password.index(password.startIndex, offsetBy: rule.min)
+		let char1 = password[idx1s..<idx1e].first
+		let idx2s = password.index(password.startIndex, offsetBy: rule.max-1)
+		let idx2e = password.index(password.startIndex, offsetBy: rule.max)
+		let char2 = password[idx2s..<idx2e].first
+
+		return char1 != char2 && (char1 == rule.character || char2 == rule.character)
 	}
 }
