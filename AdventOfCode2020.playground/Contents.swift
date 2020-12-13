1e4b8bc136fb30afc084a71518a69c7a84d39b2a
Simon Biickert <simon@biickert.ca>
2020-12-13 07:59:42 -0700

    Day 13 part 1 complete

--- AdventOfCode2020.playground/Contents.swift
+++ AdventOfCode2020.playground/Contents.swift
@@ -14,6 +14,47 @@ import Cocoa
 //Day9.solve()
 //Day10.solve()
 //Day11.solve()
+//Day12.solve()
 
-Day12.solve()
+Day13.solve()
 
+public class Day13 {
+	public static func solve() {
+		let input = readInputFile(named: "Day13_Input", removingEmptyLines: true)
+		part1(input)
+		let busSched = input[1].components(separatedBy: ",")
+		print(busSched)
+	}
+	
+	static func part1(_ input: [String]) {
+		let earliestDepartTime = Int(input[0])!
+		let busIDs = input[1].components(separatedBy: ",").compactMap({ Int($0)}).sorted()
+		print(busIDs)
+		var delay = 0
+		var timesSinceDepart = busIDs.map { earliestDepartTime % $0 }
+		while timesSinceDepart.contains(0) == false {
+			delay += 1
+			timesSinceDepart = busIDs.map { (earliestDepartTime + delay) % $0 }
+		}
+		print(timesSinceDepart)
+		let busIndex = timesSinceDepart.firstIndex(of: 0)!
+		print("Bus \(busIDs[busIndex]) is the first to depart with a delay of \(delay) minutes.")
+		print("Part 1 answer: \(busIDs[busIndex] * delay)")
+	}
+	
+	static func readInputFile(named name:String, removingEmptyLines removeEmpty:Bool) -> [String] {
+		var results = [String]()
+		if let inputPath = Bundle.main.path(forResource: name, ofType: "txt") {
+			do {
+				let input = try String(contentsOfFile: inputPath)
+				results = input.components(separatedBy: "\n")
+			} catch {
+				print("Could not read file \(name)")
+			}
+		}
+		if removeEmpty {
+			results = results.filter { $0.count > 0 }
+		}
+		return results
+	}
+}
