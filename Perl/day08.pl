0dedc1b6fdd65a6419d9af6e8ce9624457a1f1ef
Simon Biickert <simon@biickert.ca>
2022-03-23 15:44:49 -0600

    Day 8 complete in OO Perl

--- Perl/day08.pl
+++ Perl/day08.pl
@@ -1,16 +1,20 @@
 #!/usr/bin/env perl
-BEGIN {
-    use Cwd;
-    our $directory = cwd;
-}
-
-use lib $directory;
 
 package Instruction;
 	use Moose;
 	
-	has 'op' => 	(is => 'ro', isa => 'Str');
+	has 'op' => 	(is => 'rw', isa => 'Str');
 	has 'val' =>	(is => 'ro', isa => 'Int');
+	
+	sub flip_op {
+		my $self = shift;
+		if ($self->op eq 'nop') {
+			$self->op('jmp');
+		}
+		elsif ($self->op eq 'jmp') {
+			$self->op('nop');
+		}
+	}
 	
 	no Moose;
 __PACKAGE__->meta->make_immutable;
@@ -20,39 +24,62 @@ package main;
 use Modern::Perl;
 use autodie;
 use Data::Dumper;
-use Set::Scalar;
-#use AOC::Geometry qw(Point2D Line2D);
+use Storable 'dclone';
 
 my $INPUT_PATH = './input';
-my $INPUT_FILE = '08.test.txt';
-#my $INPUT_FILE = '08.challenge.txt';
+#my $INPUT_FILE = '08.test.txt';
+my $INPUT_FILE = '08.challenge.txt';
 my @program = parse_input("$INPUT_PATH/$INPUT_FILE");
 
 solve_part_one();
-#solve_part_two();
-
+solve_part_two();
 
 exit( 0 );
 
 sub solve_part_one {
-	my ($ptr, $acc) = run(@program);
+	my ($acc, @code_path) = run(@program);
 	say 'Part One';
-	say "Program was going to repeat instr at $ptr. acc is $acc.";
+	say "Program was going to repeat instr at $code_path[-1]. acc is $acc.";
 }
 
 sub solve_part_two {
+	my $step;
+	my $ptr;
+	my $instr;
+	
+	my ($acc, @code_path) = run(@program);
+	$step = $#code_path; # the last run instruction, will use to count back along code path
+	
+	while ($code_path[-1] <= $#program) {
+		# did not "go past the last instruction" and end successfully
+		#say "Ended at $code_path[-1], trying again.";
+		do {
+			$step--;
+			$ptr = $code_path[$step];
+			$instr = $program[$ptr];
+		} until ($instr->op eq 'nop' or $instr->op eq 'jmp');
+		
+		my @program_copy = @{dclone(\@program)};
+		$instr->flip_op();
+		($acc, @code_path) = run(@program_copy);
+	}
+	say 'Part Two';
+	say "acc is $acc";
 }
 
 sub run {
 	my @program = @_;
 	my $acc = 0;
 	my $ptr = 0;
-	my $visited = Set::Scalar->new();
+	my @visited;
 	
 	while (1) {
-		last if $visited->contains($ptr);
-		last if $ptr > $#program;
-		$visited->insert($ptr);
+		if (grep { $_ == $ptr } @visited or $ptr > $#program or $ptr < 0) {
+			push(@visited, $ptr);
+			last;
+		}
+		push(@visited, $ptr);
+		
 		my $instr = $program[$ptr];
 		if ($instr->op eq 'nop') {
 			$ptr++;
@@ -66,7 +93,7 @@ sub run {
 		}
 	}
 	
-	return ($ptr, $acc);
+	return ($acc, @visited);
 }
 
 sub parse_input {
