6e537913e63cbfcf474c2c97e8ea1f456034f8d1
Simon Biickert <simon@biickert.ca>
2022-11-27 22:04:01 -0700

    Day 14 part 2 complete
    Merged solving parts 1 and 2 into one function

--- Perl/day14.pl
+++ Perl/day14.pl
@@ -10,8 +10,8 @@ use lib $local_lib;
 
 use Modern::Perl 2018;
 use autodie;
-use Data::Dumper;
-#use Storable 'dclone';
+#use Data::Dumper;
+#use List::MoreUtils qw(uniq);
 use bigint;
 
 use AOC::Util;
@@ -19,49 +19,57 @@ use AOC::Util;
 
 my $INPUT_PATH = './input';
 #my $INPUT_FILE = '14.test1.txt';
+#my $INPUT_FILE = '14.test2.txt';
 my $INPUT_FILE = '14.challenge.txt';
 my @input = read_input("$INPUT_PATH/$INPUT_FILE");
 
 say "Advent of Code 2020, Day 14: Docking Data";
 
-solve_part_one(@input);
-#solve_part_two(@input);
+solve(@input);
 
 exit( 0 );
 
-sub solve_part_one {
+sub solve {
 	my @input = @_;
-	my %memory = ();
+	my %memory1 = ();
+	my %memory2 = ();
 	my @mask;
 	
 	for my $line (@input) {
 		if ( $line =~ m/^mask = ([X01]+)/ ) {
 			my $pattern = $1;
-			#say $pattern;
 			@mask = ();
 			for my $char (split('', $pattern)) {
 				unshift(@mask, $char);
 			}
-			#say join(' ', @mask);
 		}
 		elsif ( $line =~ m/^mem\[(\d+)\] = (\d+)/ ) {
 			my $addr = $1;
 			my $val = $2;
+			
+			# Part 1
 			my $decoded = decode_version_1($val, @mask);
-			#say "Set mem[$addr] to $decoded (was: $val)";
-			$memory{$addr} = $decoded;
+			$memory1{$addr} = $decoded;
+			
+			# Part 2
+			my @addresses = decode_version_2($addr, @mask);
+			for my $a (@addresses) {
+				$memory2{$a} = $val;
+			}
 		}
 	}
 	
 	my $sum = 0;
-	for my $key ( keys %memory ) {
-		$sum += $memory{$key};
+	for my $key ( keys %memory1 ) {
+		$sum += $memory1{$key};
 	}
 	say "Part 1: The sum of the memory values is $sum";
-}
-
-sub solve_part_two {
-	my @input = @_;
+	
+	$sum = 0;
+	for my $key ( keys %memory2 ) {
+		$sum += $memory2{$key};
+	}
+	say "Part 2: The sum of the memory values is $sum";
 }
 
 sub decode_version_1 {
@@ -80,4 +88,49 @@ sub decode_version_1 {
 	}
 	
 	return $value;
+}
+
+sub decode_version_2 {
+	# value is an int
+	# mask is an array of 1's, 0's and X's, but reversed
+	# so the least significant bit is first
+	my ($value, @mask) = @_;
+	my @results = ();
+	
+	# Set the 1's
+	for (my $i = 0; $i <= $#mask; $i++) {
+		if ($mask[$i] == 1) {
+			$value |= 2 ** $i;	
+		}
+	}
+	
+	# Going to convert value into array, with least sig bit first
+	my @value_array = reverse split('', sprintf('%036b', $value));
+	
+	# Get the floating bits
+	my @x_indexes = ();
+	for (my $i = 0; $i <= $#mask; $i++) {
+		if ($mask[$i] eq 'X') { push( @x_indexes, $i ); }
+	}
+	my $count = scalar grep(/X/, @mask);
+	
+	# Get the ones and zeros to substitute
+	my @nums = ();
+	for (my $n = 0; $n < 2 ** $count; $n++) {
+		my @bits = reverse split('', sprintf('%0'.$count.'b', $n));
+		push(@nums, \@bits);
+	}
+	
+	my @value_array_copy;
+	for my $b_ref (@nums) {
+		@value_array_copy = @value_array;
+		for (my $i = 0; $i <= $#x_indexes; $i++) {
+			my $x_index = $x_indexes[$i];
+			$value_array_copy[$x_index] = $b_ref->[$i];
+		}
+		my $result = oct('0b' . join('', reverse(@value_array_copy)));
+		push( @results, $result );
+	}
+	
+	return @results;
 }
